765e79321b1d8630a3cace41cd4be8b4c6cb2772
Alberto Duran <albertoduranfilho@gmail.com>
2025-12-12 11:18:10 -0300

    :buld: docs: Colocando os métodos do repository em inglês

--- src/repository/ProdutoRepository.ts
+++ src/repository/ProdutoRepository.ts
@@ -3,13 +3,13 @@ import { Produto } from "../models/Produto";
 export interface ProdutoRepository{
 
     // CRUD do Produto
-	procurarPorId(id: number): void;
-	listarTodas(): void;
-	cadastrar(produto: Produto): void;
-	atualizar(produto: Produto): void;
-	deletar(id: number): void;
+	searchById(id: number): void;
+	listAll(): void;
+	register(product: Produto): void;
+	update(product: Produto): void;
+	delete(id: number): void;
 
     // Método Específico
-    comprar(id: number, quantidade: number): void
+    by(id: number, amount: number): void
 
 }
